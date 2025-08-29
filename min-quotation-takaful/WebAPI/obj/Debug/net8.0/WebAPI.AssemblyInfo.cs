ad6cdbeddbd060f1753f544b857a4bb38130948e
mr-indonesia <secretcode190@gmail.com>
2025-08-29 10:42:09 +0700

    update controller

--- min-quotation-takaful/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ min-quotation-takaful/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa58a02b620825dba4e3aee252d597b84b7a083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fa67fd1b886b9d77ec8240038ca66ce1e4d301")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
